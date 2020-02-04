3771a2f46bc52ffdd6e3bf0939300542f436e2ca
Tiko <tikxach20@gmail.com>
2020-02-04 18:35:59 +0400

    msg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {useAuth} from "./hooks/auth.hook";
+import {AuthContext} from "./context/AuthContext";
+import {useRoutes} from './routes'
+import {NavBar} from "./components/NavBar";
+import {Loader} from "./components/Loader";
+import 'materialize-css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const {login, logOut, token, userId, ready} = useAuth();
+    const isAuthenticated = !!token;
+    const routes = useRoutes(isAuthenticated);
+
+    if(!ready){
+        return <Loader/>
+    }
+
+    return (
+        <AuthContext.Provider value={{login, logOut, token, userId, isAuthenticated}}>
+            <Router>
+                { isAuthenticated && <NavBar/> }
+                <div className="container">
+                    {routes}
+                </div>
+            </Router>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
---
+++ src/context/AuthContext.js
@@ -0,0 +1,1 @@
+import {createContext} from 'react';function foo() {}export const AuthContext = createContext({   token: null,   userId: null,   login: foo,   logOut: foo,   isAuthenticated: false});
